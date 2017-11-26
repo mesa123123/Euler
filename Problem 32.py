2b96fee4205bdd1568d1fd3850eccb8ca27634e3
mesa123123 <bowmanpete123@gmail.com>
2017-11-27 12:03:36 +1300

    Still not the right asnwer but I've made it shorter using a built in method

--- Problem 32.py
+++ Problem 32.py
@@ -1,5 +1,4 @@
-import math
-
+import itertools
 
 def checkProduct(numberList):
     if int(numberList[0]) == 4:
@@ -15,25 +14,10 @@ def checkProduct(numberList):
     else:
         return False
 
-def permutations(word):
-        if len(word) <= 1:
-            return [word]
-        # get all permutations of length N-1
-        perms = permutations(word[1:])
-        char = word[0]
-        result = []
-        # iterate over all permutations of length N-1
-        for perm in perms:
-            # insert the character into every possible location
-            for i in range(len(perm) + 1):
-                result.append(perm[:i] + char + perm[i:])
-        return result
-
-
 def PandigitalIdentities():
     numbers = list(range(1,10))
     sum = 0
-    numbersPerms = permutations(str(''.join(map(str,numbers))))
+    numbersPerms = itertools.permutations(numbers)
     for numbers in numbersPerms:
         if checkProduct(list(numbers)):
             sum += int(''.join(map(str, numbers[5:])))
