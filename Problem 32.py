cdb2a3a9f893b0ab328c4c21bee77bb83128f30b
mesa123123 <bowmanpete123@gmail.com>
2017-11-27 11:57:33 +1300

    Added Work on Problem 32; the permutations algorithm was borrowed and I'm still not getting the right answer!

--- Problem 32.py
+++ Problem 32.py
@@ -1,17 +1,45 @@
+import math
+
+
+def checkProduct(numberList):
+    if int(numberList[0]) == 4:
+        lMultiplier = int(''.join(map(str,numberList[1:5])))
+        lResult = int(''.join(map(str,numberList[5:])))
+        if 4*lMultiplier == lResult:
+            return True
+    Mulitplicand = int(''.join(map(str, numberList[:2])))
+    Multiplier = int(''.join(map(str, numberList[2:5])))
+    Result = int(''.join(map(str, numberList[5:])))
+    if Mulitplicand * Multiplier == Result:
+        return True
+    else:
+        return False
+
+def permutations(word):
+        if len(word) <= 1:
+            return [word]
+        # get all permutations of length N-1
+        perms = permutations(word[1:])
+        char = word[0]
+        result = []
+        # iterate over all permutations of length N-1
+        for perm in perms:
+            # insert the character into every possible location
+            for i in range(len(perm) + 1):
+                result.append(perm[:i] + char + perm[i:])
+        return result
+
+
 def PandigitalIdentities():
     numbers = list(range(1,10))
-    result = 0
-    for k in range(10,100):
-        for m in range(100,1000):
-            prod2 = k*m
-            if len(str(prod2)) == 4:
-                 identity = [int(d) for d in str(str(k)+ str(m)+ str(prod2))]
-                 identity.sort()
-                 if identity == numbers:
-                     print(str(k) + " " + str(m) + " " + str(prod2))
-                     result += prod2
-    return result
+    sum = 0
+    numbersPerms = permutations(str(''.join(map(str,numbers))))
+    for numbers in numbersPerms:
+        if checkProduct(list(numbers)):
+            sum += int(''.join(map(str, numbers[5:])))
+    print("Sum is: " + str(sum))
 
-print(PandigitalIdentities())
+
+PandigitalIdentities()
 
 #correct answer is 45228
